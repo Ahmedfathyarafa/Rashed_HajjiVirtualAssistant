dd99f26d6a968d3c1ddcc96f967e7fea9d85b472
MahmoudElwakeil <mahmoudwakeil@gmail.com>
2018-08-02 19:39:27 +0300

    Places Location API

---
+++ Rashed_PlacesLocationAPI/RashidApi/Controllers/locationController.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RashidApi.Models;
+
+namespace RashidApi.Controllers
+{
+    public class locationController : ApiController
+    {
+        public async System.Threading.Tasks.Task<string> getLocations(string querytxt,string lat,string lng) //(string latitude,string longitude)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetStringAsync(string.Format("https://maps.googleapis.com/maps/api/place/textsearch/json?query="+querytxt+"&location={0},{1}&radius=500&key=AIzaSyDgrz4Rf3AX5bSfytgjbrwnXRs46qNRXdA", lat, lng));
+
+              //  var result = JsonConvert.DeserializeObject<placesApiQueryResponse>(response).ToString();
+                return response;
+            }
+        }
+
+    }
+
+}
---
+++ Rashed_PlacesLocationAPI/RashidApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RashidApi.WebApiApplication" Language="C#" %>
